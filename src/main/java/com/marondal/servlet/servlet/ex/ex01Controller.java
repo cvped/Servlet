21100bbb19a11344c9de1f7250430c42465443f1
cvped <stay488@gmail.com>
2024-10-21 18:47:50 +0900

    Update ex01Controller.java

--- src/main/java/com/marondal/servlet/servlet/ex/ex01Controller.java
+++ src/main/java/com/marondal/servlet/servlet/ex/ex01Controller.java
@@ -2,4 +2,12 @@ package com.marondal.servlet.servlet.ex;
 
 public class ex01Controller {
 
+  	@Override
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		
+		PrintWriter out = response.getWriter();
+		
+		out.println("hello world!!");
+	}
+
 }
